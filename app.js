fa91a68c50c83044d5926f47c560eee73ecafa2b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2021-03-29 11:56:44 -0700

    Testing EJS

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const PORT = 3000;
 
+// Middleware
+app.set('view engine', 'ejs');
+app.set('view engine', path.join(__dirname, '/views'));
+
 app.get('/', (req, res) => {
-    res.send('HIIIII')
+    res.render('home.ejs')
 });
 
 app.listen(PORT, () => {
