15e901c781bb04acaf8e079dfd05087c89b63b83
Brian Nguyen <brian.edison.nguyen@gmail.com>
2021-03-31 12:09:08 -0700

    middleware to load public files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + "/public"));
 
 // Routes
 app.get('/', (req, res) => {
