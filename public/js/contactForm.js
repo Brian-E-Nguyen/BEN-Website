53e969392ce0b4ee793512e34d3bc7bfa54553a2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2021-04-14 11:32:49 -0700

    retrieving form inputs' values

--- public/js/contactForm.js
+++ public/js/contactForm.js
@@ -1,7 +1,19 @@
 const contactForm = document.querySelector('.contact-form');
 
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('emailInput');
+const message = document.getElementById('message');
+
 contactForm.addEventListener('submit', (e) => {
     // Prevents page from being refreshed
     e.preventDefault();
-    console.log('Submitted!!!')
-})
+    var formData = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        message: message.value
+    }
+    console.log(formData)
+});
+            
