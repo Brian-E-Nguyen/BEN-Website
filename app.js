b1d293cb359f36420ecff5fdce7673b9365b8d21
Brian Nguyen <brian.edison.nguyen@gmail.com>
2021-04-14 11:25:06 -0700

    changed PORT variable to include process.env.PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ejsMate = require('ejs-mate');
 const path = require('path')
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.engine('ejs', ejsMate);
