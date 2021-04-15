e9a9d0ffc65cfee5d9a35cbd2a0a658ae08733a1
Brian Nguyen <brian.edison.nguyen@gmail.com>
2021-04-14 19:37:02 -0700

    separate functions for form data validations

--- public/js/formValidation.js
+++ public/js/formValidation.js
@@ -1,4 +1,4 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// Bootstrap validation styles
 (function() {
     'use strict';
     window.addEventListener('load', function() {
@@ -17,7 +17,27 @@
     }, false);
   })();
 
-  function validateEmail(email) {
+function validateFormData(formData) {
+  const hasNullOrWhitespace = checkNullOrWhitespace(formData);
+  if(!hasNullOrWhitespace) {
+    const isValidEmail = validateEmail(formData['email']);
+    return (isValidEmail ? true : false);
+  }
+  return false;
+}
+
+// Email validations
+function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+}
+
+function checkNullOrWhitespace(formData) {
+  for (const key in formData) {
+    var data = formData[key];
+    if(data === null || data.match(/^ *$/) !== null) {
+        hasValidData = false;
+        break;
+    }
+  }
 }
