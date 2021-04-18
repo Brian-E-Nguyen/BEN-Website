e5f38ed8d501b90e28fe0bd69b620275bb1cf789
Brian Nguyen <brian.edison.nguyen@gmail.com>
2021-04-18 13:02:13 -0700

    app.js: added controllers to project routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ejsMate = require('ejs-mate');
 const path = require('path');
 const app = express();
 const mainView = require('./controllers/mainView')
+const projectView = require('./controllers/projectView')
 const PORT = process.env.PORT || CONFIG.localPort;
 
 // Middleware
@@ -16,33 +17,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
-// Routes
+// Main Views
 app.get('/', mainView.home);
 
 app.get('/about-me', mainView.about);
 
 app.get('/portfolio', mainView.portfolio);
-
-// Projects
-app.get('/yelpcamp', (req, res) => {
-    res.render('projects/yelpcamp.ejs', {title: 'BEN | YelpCamp'})
-});
-
-app.get('/personal-website', (req, res) => {
-    res.render('projects/personal-website.ejs', {title: 'BEN | My Website'})
-});
-
-app.get('/kraken-bracket', (req, res) => {
-    res.render('projects/kraken-bracket.ejs', {title: 'BEN | Kraken Bracket'})
-});
-
-app.get('/ee-381', (req, res) => {
-    res.render('projects/ee-381.ejs', {title: 'BEN | EE-381'})
-});
-
-app.get('/simd', (req, res) => {
-    res.render('projects/simd.ejs', {title: 'BEN | SIMD'})
-});
 
 app.get('/resume', mainView.resume);
 
@@ -51,6 +31,17 @@ app.get('/photography', mainView.photography);
 app.get('/contact', mainView.contact);
 
 app.post('/contact', mainView.contactFormPost)
+
+// Projects
+app.get('/yelpcamp', projectView.yelpcamp);
+
+app.get('/personal-website', projectView.personalwebsite);
+
+app.get('/kraken-bracket', projectView.krakenbracket);
+
+app.get('/ee-381', projectView.ee381);
+
+app.get('/simd', projectView.simd);
 
 app.all('*', (req, res) => {
     res.render('error.ejs', {title: 'Error!'})
