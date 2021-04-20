3f971b1e7a3f9a953a9c3003fafb9aa511faa627
Brian Nguyen <brian.edison.nguyen@gmail.com>
2021-04-20 13:11:32 -0700

    init script and style src urls

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const CONFIG = require('./config.json');
 const express = require('express');
 const ejsMate = require('ejs-mate');
 const path = require('path');
+const helmet = require('helmet');
 const app = express();
 const mainView = require('./controllers/mainView');
 const projectView = require('./controllers/projectView');
@@ -17,6 +18,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
+
+// Security
+const scriptSrcUrls = [
+    'https://code.jquery.com/',
+    'https://maxcdn.bootstrapcdn.com/'
+]
+
+const styleSrcUrls = [
+    'https://kit.fontawesome.com/',
+    'https://cdnjs.cloudflare.com/'
+]
 
 // Main Views
 app.get('/', mainView.home);
